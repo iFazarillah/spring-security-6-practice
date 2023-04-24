3cb84689530e24dcd9f28c7c942c0922c93fde79
iFazarillah <fazarillah@gmail.com>
2023-04-24 17:45:10 +0700

    initial project

---
+++ src/main/java/com/fazarillah/practice/PracticeApplication.java
@@ -0,0 +1,16 @@
+package com.fazarillah.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("APLIKASI BERJALAN");
+        SpringApplication.run(PracticeApplication.class, args);
+
+    }
+
+}
